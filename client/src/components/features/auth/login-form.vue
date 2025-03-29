24f6978818863640883da497396d5f8f704a534f
eaaaarl <dev.earl00@gmail.com>
2025-03-29 11:34:48 +0800

    feat: add VeeValidate and Zod for input validation

--- client/src/components/features/auth/login-form.vue
+++ client/src/components/features/auth/login-form.vue
@@ -7,8 +7,29 @@ import {
   CardHeader,
   CardTitle,
 } from "@/components/ui/card";
+import {
+  FormControl,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from "@/components/ui/form";
 import { Input } from "@/components/ui/input";
 import { Label } from "@/components/ui/label";
+import { loginSchema } from "@/schema/login.schema";
+import { useForm } from "vee-validate";
+
+const form = useForm({
+  validationSchema: loginSchema,
+  initialValues: {
+    email: "",
+    password: "",
+  },
+});
+
+const onSubmit = form.handleSubmit((values) => {
+  console.log("Form submitted!", values);
+});
 </script>
 
 <template>
@@ -20,27 +41,51 @@ import { Label } from "@/components/ui/label";
       </CardDescription>
     </CardHeader>
     <CardContent>
-      <div class="grid gap-4">
-        <div class="grid gap-2">
-          <Label for="email">Email</Label>
-          <Input id="email" type="email" placeholder="m@example.com" required />
-        </div>
-        <div class="grid gap-2">
-          <div class="flex items-center">
-            <Label for="password">Password</Label>
-            <a href="#" class="ml-auto inline-block text-sm underline">
-              Forgot your password?
-            </a>
+      <form @submit="onSubmit">
+        <div class="grid gap-4">
+          <div class="grid gap-2">
+            <FormField v-slot="{ componentField }" name="email">
+              <FormItem>
+                <FormLabel>Email</FormLabel>
+                <FormControl>
+                  <Input
+                    type="email"
+                    placeholder="Email"
+                    v-bind="componentField"
+                  />
+                </FormControl>
+                <FormMessage />
+              </FormItem>
+            </FormField>
           </div>
-          <Input id="password" type="password" required />
+          <div class="grid gap-2">
+            <div class="flex items-center">
+              <Label for="password">Password</Label>
+              <a href="#" class="ml-auto inline-block text-sm underline">
+                Forgot your password?
+              </a>
+            </div>
+            <FormField v-slot="{ componentField }" name="password">
+              <FormItem>
+                <FormControl>
+                  <Input
+                    type="password"
+                    placeholder="Enter your password"
+                    v-bind="componentField"
+                  />
+                </FormControl>
+                <FormMessage />
+              </FormItem>
+            </FormField>
+          </div>
+          <Button type="submit" class="w-full"> Login </Button>
+          <Button variant="outline" class="w-full"> Login with Google </Button>
         </div>
-        <Button type="submit" class="w-full"> Login </Button>
-        <Button variant="outline" class="w-full"> Login with Google </Button>
-      </div>
-      <div class="mt-4 text-center text-sm">
-        Don't have an account?
-        <a href="#" class="underline"> Sign up </a>
-      </div>
+        <div class="mt-4 text-center text-sm">
+          Don't have an account?
+          <a href="#" class="underline"> Sign up </a>
+        </div>
+      </form>
     </CardContent>
   </Card>
 </template>
--- client/src/main.ts
+++ client/src/main.ts
@@ -2,7 +2,10 @@ import { createApp } from "vue";
 import App from "./App.vue";
 import router from "./router";
 import "./assets/index.css";
+import { createPinia } from "pinia";
 
 const app = createApp(App);
+const pinia = createPinia();
+app.use(pinia);
 app.use(router);
 app.mount("#app");
